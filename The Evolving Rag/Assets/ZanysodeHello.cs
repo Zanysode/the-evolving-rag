aa89c3d13b7379b7a8ef993f8864377e67351182
Zanosode Organization <zanosode@gmail.com>
2020-03-28 07:53:07 -0300

    Created first script
    A simple Hello World to test the game's code.

---
+++ The Evolving Rag/Assets/ZanysodeHello.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZanysodeHello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World! Welcome to 'The Evolving Rag'.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
